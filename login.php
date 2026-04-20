5ad62eda4850687d795ceef916c66330cbf549d2
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-04-20 17:06:30 +0200

    add link zu signup Page for new User

--- login.php
+++ login.php
@@ -1,4 +1,10 @@
-<?php
+<form action="" method ="post">
+	<label for="user">user name</label>
+	<input type= "text" name ="user" id ="user">
+	<label for="password">password</label>
+	<input type= "password" name ="password" id ="password">
+    <button type = "submit" value ="login" name = "submit"> Login </button>
+    
+</form>
+<span > Don't have an account? <a href = "signup.php">Sign up</a></span>
 
-?>
-<button type = "button" onclick = "window.location.href = 'pages/home.php'"> Login </button>
