875695065cb41049e6a8683f6d1a32f06d489610
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-04-20 14:03:04 +0200

    create Template with semantics tags

---
+++ tools/pages_template.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Template</title>
+	<link rel = "stylesheet" href = "css/style.css">
+	<style></style>
+	<script src = "js/script.js"></script>
+</head>
+<body>
+	
+	<header><h1>VorlTemplate</h1></header>
+
+	<nav></nav>
+
+	<main></main>
+	
+	<footer></footer>
+	
+</body>
+</html>
