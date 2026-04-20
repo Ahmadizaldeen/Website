3ffe8dc74939fbb5185a6e34a4f3273d9f967457
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-04-20 13:54:32 +0200

    Add tools folder

--- tools/make_folders_website.bat
+++ tools/make_folders_website.bat
@@ -1,6 +1,6 @@
 mkdir %1
 cd %1
-mkdir css js pages data include bilder test
+mkdir css js pages data include bilder test tools
 /*in cmd type: make_folders_website.bat my_website
 This will create a folder named my_website with the following subfolders: css, js, include, bilder, and test.
 You can replace "my_website" with any name you want for your project folder.*/
