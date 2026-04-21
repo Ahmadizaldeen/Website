74c634e442cbff9afdd353947cef672ddb30f02e
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-04-21 23:14:56 +0200

    Refactor signup process to handle errors with messages instead of exit statements

--- signup.php
+++ signup.php
@@ -2,6 +2,8 @@
 session_start(); // für automatische Anmeldung nach dem signup
 $users_data = 'data/users.txt';
 require_once "tools/validation.php";
+require_once "tools/user_data_administration.php";
+$error_message ="";
 if(isset($_POST['submit'])){
 	if(!empty($_POST['firstname'])&& !empty($_POST['lastname'])
         && !empty($_POST['user_name']) && !empty($_POST['password'])){
@@ -11,17 +13,15 @@ if(isset($_POST['submit'])){
 		$password = clean_password($_POST['password']);//ohne trim für das Password
         $salutation = clean_input($_POST['salutation']) ?? null;
         $confirm_password =clean_password( $_POST['confirm_password']);
-
         if ($password != $confirm_password){
-            echo 'passwords do not match';
-            exit;
+            $error_message = 'passwords do not match';
         }
-        require_once "tools/validation.php";
-        $is_valid = validate_user ($user_name, $password);
-        if (!$is_valid){
-            echo "Invalid user name or password";
-            echo "User name must start with a letter and be 4-8 characters long. Password must be 6-8 characters long and can contain letters, numbers, and underscores.";
-            exit;
+        elseif (!validate_user ($user_name, $password)){
+            $error_message = "Invalid user name or password <br>
+            User name must start with a letter and be 4-8 characters long. Password must be 6-8 characters long and can contain letters, numbers, and underscores.";         
+        }
+        elseif (user_exists($users_data, $user_name)){
+               $error_message = "user already exists, please choose a different user name";
         }
         else{
                 $user_data = array(
@@ -29,26 +29,16 @@ if(isset($_POST['submit'])){
                 'firstname' => $firstname,
                 'lastname' => $lastname,
                 'user_name' => $user_name,
-                'password' => password_hash($password, PASSWORD_DEFAULT)
-            );
-            require_once "tools/user_data_administration.php";
-            if (user_exists($users_data, $user_name)){
-                echo "user already exists, please choose a different user name";
-                exit;
-            }
-            else {
+                'password' => password_hash($password, PASSWORD_DEFAULT));
                 save_user($users_data, $user_data);
                 header("Location: login.php"); // nach dem Speichern zu login
                 exit;
             }
-
-        }
-        }
-        else {
-            echo "please fill in all fields";
-            }
-        }
-
+    }
+    else {
+        $error_message = "please fill in all fields";
+    }
+}
 ?>
 
 <form action="" method ="post">
@@ -68,6 +58,9 @@ if(isset($_POST['submit'])){
 	<input type= "password" name ="password" id ="password">
     <label for = "confirm_password">Confirm Password :</label>
     <input type= "password" name ="confirm_password" id ="confirm_password"><br>
+    <?php if ($error_message): ?>
+        <p style="color: red;"><?= $error_message ?></p>
+    <?php endif; ?>
     <button type = "submit" value ="signup" name = "submit" >Create Account</button>
     
 </form>
