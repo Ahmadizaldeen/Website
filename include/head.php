f8a3e540e364968022a6f0cae56e749b4220be2d
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-04-22 15:52:36 +0200

    Function for dynamic header and title

--- include/head.php
+++ include/head.php
@@ -1,9 +1,13 @@
+<?php
+require_once __DIR__."/../tools/get_filename_as_title.php";
+$title = get_file_name();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Home Page</title>
+	<title><?=$title?></title>
 	<link rel = "stylesheet" href = "css/style.css">
 	<style></style>
 	<script src = "js/script.js"></script>
--- include/header.php
+++ include/header.php
@@ -1,1 +1,4 @@
-<header><h1>Contact</h1></header>
+<?php require_once __DIR__."/../tools/get_filename_as_title.php";
+$header = get_file_name();
+?>
+<header><h1><?=$header?></h1></header>
