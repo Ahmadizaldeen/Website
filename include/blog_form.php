e72bf5b580fc7d8f580bfe96b8edc2a8446de054
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-04-24 01:30:57 +0200

    Add form for input

---
+++ include/blog_form.php
@@ -0,0 +1,8 @@
+
+<form action="" method ="post">
+	<label for="user">title :</label>
+	<input type= "text" name ="title" id ="title" ><br><br>
+	<label for="password">Inhalt</label>
+	<textarea name ="inhalt" id ="inhalt" rows="6" cols ="20"></textarea><br><br>
+    <button type = "submit" name = "save"> save </button>
+</form>
