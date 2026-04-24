542bf8cced6439a5b396579cc0b6bc6903813022
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-04-24 02:12:25 +0200

    bug: fix file name

--- include/user_tools.php
+++ include/user_tools.php
@@ -9,6 +9,6 @@ require_once __DIR__ . "/config.php";
 <span><?="Hallo " . $_SESSION['user_data']['firstname'] . " ". $_SESSION['user_data']['lastname']?></span>
 <a href = "<?= BASE_URL ?>/pages/user_pages/create_post.php">Create Post</a>
 <a href = "<?= BASE_URL ?>/pages/user_pages/setting.php">Setting</a>
-<a href = "<?= BASE_URL ?>/pages/user_pages/static.php">static</a>
+<a href = "<?= BASE_URL ?>/pages/user_pages/statistic.php">Statistics</a>
 <a href = "<?= BASE_URL ?>/pages/user_pages/signout.php">signout</a>
 <hr>
