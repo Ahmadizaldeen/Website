86206cf8577a05d42e5c49bc81f9be51680d6b43
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-04-24 04:39:59 +0200

    hidden user tools wenn the visitor no account has

--- pages/agb.php
+++ pages/agb.php
@@ -9,7 +9,7 @@ require_once __DIR__."/../include/header.php";
 	<?php require_once __DIR__ . "/../include/navigation.php"; ?>
 	<?php if(isset($_SESSION['user_data']) && $_SESSION['user_data'])
 		require_once "../include/user_tools.php"; 
-	else require_once __DIR__ . "/../include/user_tools.php"; ?>
+	 ?>
 </nav>
 
 <main>
