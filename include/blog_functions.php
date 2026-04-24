898b9060bd102c423d7ec563705212f060e9601a
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-04-24 02:20:55 +0200

    move form processing to blog_form

--- include/blog_functions.php
+++ include/blog_functions.php
@@ -3,31 +3,6 @@ require_once __DIR__."/../include/if_session.php";
 require_once __DIR__."/../tools/validation.php";
 require_once __DIR__."/../include/config.php";
 ?>
-
-<?php # form-Data handling function 
-$autor = $_SESSION['user_data']['firstname'] . " " . $_SESSION['user_data']['lastname'];
-$blogs_file = __DIR__ . "/../data/blogs.txt";
-
-if(isset($_POST['save'])){
-	if(!empty($_POST['title'])&& !empty($_POST['inhalt'])){
-        $title = clean_input($_POST['title']);
-        $inhalt = clean_input($_POST['inhalt']);
-        $blog_data = array(
-            'user_name' => $_SESSION['user_data']['user_name'],
-            'autor' => $autor,
-            'title' => $title,
-            'inhalt' => $inhalt,
-            'timestamp_id' => time()
-        );
-        save_post($blogs_file, $blog_data);
-        header("Location: ".BASE_URL."/pages/home.php");
-        exit;
-        }
-        else {
-            $error_message = "please fill in all fields";
-        }
-}
-?>
 <?php
 function save_post($blogs_file, $blog_data ){
     $user_name = $blog_data['user_name'];
