e7df1755dc37f618a358a2074e747d50faa1cc0b
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-04-24 05:01:39 +0200

    styling

--- include/footer_navi.php
+++ include/footer_navi.php
@@ -1,8 +1,8 @@
 <?php require_once __DIR__ . "/config.php"; ?>
 
-<hr>
+<nav>
 <a href = "<?= BASE_URL ?>/pages/agb.php">Terms and Conditions</a>
 <a href = "<?= BASE_URL ?>/pages/imprint.php">Imprint</a>
 <a href = "<?= BASE_URL ?>/pages/contact.php">Contact</a>
 <a href = "<?= BASE_URL ?>/pages/about.php">About</a>
-<hr>
+</nav>
