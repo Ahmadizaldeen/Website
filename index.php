306598b9f76426f4a8cbe972b53c78182a9e3d41
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-04-24 03:03:14 +0200

    add index.php aus templet and connect it with login script

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__."/include/if_session.php";
+require_once __DIR__."/include/config.php";
+require_once __DIR__."/include/head.php";
+require_once __DIR__."/include/header.php";
+?>
+
+<nav> 
+	<?php require_once __DIR__."/include/navigation.php"; ?>
+	<?php if(isset($_SESSION['user_data']) && $_SESSION['user_data'])
+		require_once __DIR__."/include/user_tools.php"; 
+	else echo "<a href ='".BASE_URL."/login.php'>Login</a>"; ?>
+		
+</nav>
+
+<main>
+    <?php require_once __DIR__."/login.php";?>
+</main>
+<?php require_once __DIR__."/include/footer.php";?>	
