7792cb47c049c27c28dd600bda404c80ec9f5569
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-04-26 13:02:43 +0200

    refactor: update BASE_URL definition to use dynamic server configuration

--- include/config.php
+++ include/config.php
@@ -1,3 +1,11 @@
 <?php
-define('BASE_URL', '/fag57/Website');
+$_scheme = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' ? 'https' : 'http';
+
+$_root = rtrim(str_replace('\\', '/', $_SERVER['DOCUMENT_ROOT']), '/');
+
+$_dir = str_replace('\\', '/', dirname(__DIR__));
+
+define('BASE_URL', $_scheme . '://' . $_SERVER['HTTP_HOST'] . str_replace($_root, '', $_dir));
+
+unset($_scheme, $_root, $_dir);
 ?>
